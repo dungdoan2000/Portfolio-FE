5f44f4b90b687ed36fc8d4bdba2a9c08329f64b8
dungdoan2000 <dung.doan@marahub.com>
2023-09-12 22:27:10 +0700

    homepage

---
+++ main.js
@@ -0,0 +1,14 @@
+// window.addEventListener('DOMContentLoaded', function() {
+//     const button = document.querySelector('.mail-and-menu')
+//     console.log(button)
+//     function updateButtonPosition() {
+//         const windowHeight = this.window.innerHeight;
+//         const buttonHeight = button.clientHeight;
+
+//         button.style.top = (windowHeight - buttonHeight) + 'px'
+//     }
+
+//     updateButtonPosition();
+
+//     this.window.addEventListener('resize', updateButtonPosition)
+// })
