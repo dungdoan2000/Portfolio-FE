396555989c9928c106595f9079e70d8edef7b0c2
quocdung2000 <dung.doan@marahub.com>
2023-09-15 17:04:56 +0700

    create work page & animation for home

--- main.js
+++ main.js
@@ -1,14 +1,51 @@
-// window.addEventListener('DOMContentLoaded', function() {
-//     const button = document.querySelector('.mail-and-menu')
-//     console.log(button)
-//     function updateButtonPosition() {
-//         const windowHeight = this.window.innerHeight;
-//         const buttonHeight = button.clientHeight;
+let carQuardian = document.querySelector('.homepage_carquardian')
+let cursor = carQuardian.querySelector('.cursor')
 
-//         button.style.top = (windowHeight - buttonHeight) + 'px'
-//     }
 
-//     updateButtonPosition();
+carQuardian.addEventListener('mousemove', (e)=>{
+    const x = e.clientX - carQuardian.getBoundingClientRect().left;
+    const y = e.clientY - carQuardian.getBoundingClientRect().top;
+    console.log(x)
 
-//     this.window.addEventListener('resize', updateButtonPosition)
-// })
+    cursor.style.left = `${x}px`;
+    cursor.style.top = `${y}px`;
+})
+
+
+
+
+function randomPosition(e) {
+    const maxX = window.innerWidth - 200;
+    const maxY = window.innerHeight - 200;
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+
+    e.style.left = `${randomX}px`
+    e.style.top = `${randomY}px`
+}
+
+
+// let square1 = document.querySelector('.square1')
+// let square2 = document.querySelector('.square2')
+
+// function moveSquare() {
+//     setInterval(()=>{
+//         randomPosition(square1)
+//         randomPosition(square2)
+//     },1000)
+// }
+
+// moveSquare();
+
+let home = document.querySelector('.homepage')
+let mail = document.querySelector('.mail')
+
+let work = document.querySelector('.detail-page')
+let cardDetail = document.querySelectorAll('.card-detail')
+cardDetail.forEach((e)=>{
+    e.onclick = function() {
+        work.classList.add('active')
+        home.classList.remove('active')
+    }
+})
+
